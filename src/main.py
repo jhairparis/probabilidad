d1794571065dc7e7ab2052b136ee95723d580e95
Jhair Paris <70655025+jhairparis@users.noreply.github.com>
2023-08-23 09:43:40 -0500

    while

--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
 from lib.read_file import read_excel
 from lib.tables import create_table_qualitative, create_table_quantitative
-from question import questions_settings
+from question import questions_settings, yesOrNo
 import matplotlib.pyplot as plt
 
 
-base = read_excel()
-settings = questions_settings(base["headers"])
-print(settings)
+exit = False
+while not exit:
+    base = read_excel()
+    settings = questions_settings(base["headers"])
 
-for i in settings["qualitative"]:
-    nominal = settings["qualitative"][i]
-    t = create_table_qualitative(i, not nominal, base["data"], settings)
+    for i in settings["qualitative"]:
+        nominal = settings["qualitative"][i]
+        t = create_table_qualitative(i, not nominal, base["data"], settings)
 
-    print("\n Tabla de frecuencias de " + i)
-    print(t)
+        print("\n Tabla de frecuencias de " + i)
+        print(t)
 
-    # Todo: Make Graph
+        # Todo: Make Graph
 
-for i in settings["quantitative"]:
-    discrete = settings["quantitative"][i]
-    t = create_table_quantitative(i, discrete, base["data"], settings)
+    for i in settings["quantitative"]:
+        discrete = settings["quantitative"][i]
+        t = create_table_quantitative(i, discrete, base["data"], settings)
 
-    print("\n Tabla de frecuencias de " + i)
-    print(t)
+        print("\n Tabla de frecuencias de " + i)
+        print(t)
 
-    # Todo: Make Graph
+        # Todo: Make Graph
+
+    exit = yesOrNo("Desea salir?")
