666ae49c51beaa5043b25136b7c323ece1cc7656
Jhair Paris <70655025+jhairparis@users.noreply.github.com>
2023-08-23 22:53:40 -0500

    fix

--- src/lib/notGrouped/harmonic_mean.py
+++ src/lib/notGrouped/harmonic_mean.py
@@ -1,4 +1,5 @@
 from pandas import DataFrame
 
+
 def harmonic_mean_no_grouped(base: DataFrame, column: str):
-    return base[column].count()/sum(1/base[column])
+    return base[column].count() / sum(1 / base[column])
